d7078d5a92950afa029f1d689fcca6cebf7919b4
jojo-ana <joanah127@gmail.com>
2025-04-11 14:23:05 -0700

    some coding

--- studio1/script.js
+++ studio1/script.js
@@ -1,3 +1,11 @@
 (function(){
     'use strict';
+
+    const myVideo = document.querySelector('#myVideo');
+
+    const loading = document.querySelector('.fa-water');
+    const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
+    const line4 = document.querySelector('#line1');
 })();
