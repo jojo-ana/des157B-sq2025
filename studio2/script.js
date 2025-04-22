369150b3cc323e7200fdd5621a7e921aa79b4efd
jojo-ana <joanah127@gmail.com>
2025-04-21 23:26:04 -0700

    studio 2 HTML/CSS start

---
+++ studio2/script.js
@@ -0,0 +1,10 @@
+/* things to code: form input paired with pulling from JSON data and replacing the in the html
+    - change in time start
+    - change in time end
+    - change in description
+
+
+when the data is changed from phone to PC and vice versa => changes img source/hover img source
+    - mouseover/mouseout to change the image.src
+
+*/
